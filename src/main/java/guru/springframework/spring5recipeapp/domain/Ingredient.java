7afc44be750c19e07d3d47746b9b2d10dd04bf48
R <r@home.com>
2020-05-21 15:28:41 -0700

    Added Guac and Spicy Tacos Recipes.

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -18,6 +18,19 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/guru/springframework/spring5recipeapp/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/RecipeRepository.java
@@ -3,6 +3,6 @@ package guru.springframework.spring5recipeapp.repositories;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecipeRopository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
 }
