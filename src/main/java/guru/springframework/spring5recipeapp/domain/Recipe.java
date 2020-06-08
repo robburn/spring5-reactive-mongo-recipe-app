9862dc36c3a630e8a9cb33dbdb2a48d4854f72e8
R <r@home.com>
2020-06-08 11:28:40 -0700

    Adding New Ingredient functionality.

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -2,12 +2,15 @@ package guru.springframework.spring5recipeapp.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Recipe {
